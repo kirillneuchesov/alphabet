9b60a82c5495e603b97c69887013825ad722f201
jack hammer <jhammer@localhost.localdomain>
2018-06-04 16:25:35 +0700

    Fixing

--- src/Func.hpp
+++ src/Func.hpp
@@ -1,7 +1,7 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
-int Gaps (char *TextMas, char *c, char WordsMas[100][50], int &words);
-void Sort (char WordsMas[100][50], int &words);
+int Gaps(char *TextMas, char *c, char WordsMas[100][50], int &words);
+void Sort(char WordsMas[100][50], int &words);
 
 #endif
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,32 +1,66 @@
 #define CTEST_MAIN
 
-#include <ctest.h>
+#include <cassert>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "Func.hpp"
 
-char c[4] = {",", " ", ".", "\n"};
-
-CTEST(Gaps, CorrestGaps)
+void test1()
 {
-    int words = 0;
-    char TextMas[] = {"Prove yourself"};
+    char TextMas[] = {"prove yourself"};
+    char c[] = {" ,.\n"};
     char WordsMas[100][50];
-    int result = Gaps (TextMas, c, WordsMas, words);
-    int expected = 2;
-    ASSERT_EQUAL (expected, result);
+    int words = 0;
+    assert(Gaps(TextMas, c, WordsMas, words) == 2);
+    printf("TEST1-OK\n");
 }
 
-CTEST(Gaps, IncorrestGaps)
+void test2()
 {
-    int words = 0;
-    char TextMas[] = {"Prove!yourself"};
+    char TextMas[] = {"prove!yourself"};
+    char c[] = {" ,.\n"};
     char WordsMas[100][50];
-    int result = Gaps (TextMas, c, WordsMas, words);
-    int expected = 1;
-    ASSERT_EQUAL (expected, result);
-} 
+    int words = 0;
+    assert(Gaps(TextMas, c, WordsMas, words) == 1);
+    printf("TEST2-OK\n");
+}
 
-int main (int argc, const char** argv) 
+void test3()
 {
-    int test_res = ctest_main (argc, argv);
-    return test_res;
+    char TextMas[] = {" "};
+    char c[] = {" ,.\n"};
+    char WordsMas[100][50];
+    int words = 0;
+    assert(Gaps(TextMas, c, WordsMas, words) == 0);
+    printf("TEST3-OK\n");
+}
+
+void test4()
+{
+    char TextMas[] = {"prove yourself, and. be,honest"};
+    char c[] = {" ,.\n"};
+    char WordsMas[100][50];
+    int words = 0;
+    assert(Gaps(TextMas, c, WordsMas, words) == 5);
+    printf("TEST4-OK\n");
+}
+
+void test5()
+{
+    char TextMas[] = {"proveyourself"};
+    char c[] = {" ,.\n"};
+    char WordsMas[100][50];
+    int words = 0;
+    assert(Gaps(TextMas, c, WordsMas, words) == 1);
+    printf("TEST5-OK\n");
+}
+
+int main(int argc, const char** argv) 
+{
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
 }   
