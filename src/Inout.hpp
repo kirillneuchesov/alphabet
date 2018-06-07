b80d52a81f1c17606afd537a6e98e1b6b6b81176
kirillneuchesov <36182439+kirillneuchesov@users.noreply.github.com>
2018-06-07 21:54:02 +0700

    Delete files

--- src/Inout.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS
-#define FUNCTIONS
-
-void Menu_1();
-void Menu_2(int &v, int &k);
-void Output(char WordsMas[100][50], char *c, int k, int v, int &words);
-void Input(char *TextMas, int &k);
-
-#endif
