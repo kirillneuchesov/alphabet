bcad0a492968a8188e30630c9d92e6e2e2af734c
Alikhan Khasiev <khasiev.1999@mail.ru>
2018-06-21 22:41:00 +0700

    Fixing Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,28 +1,19 @@
-#include <fstream>
-#include <iostream>
-#include <conio.h>
-#include <cstring>
+#include <stdlib.h>
 #include <locale.h>
-#include "Inout.hpp"
-#include "Logic.hpp"
+#include "Func.hpp"
 
 int main()
 {
-    system("chcp 1251");
-    system("cls");
-    char TextMas[9999],WordsMas[100][50],c[4]={',' , ' ' , '.' , '\n'};
-    int k=0,v=0,words=0;
-    while(1)
-    {
-        Menu_1();
-        Input(TextMas);
-        Menu_2(v,k);
-        Gaps(TextMas,c, WordsMas,words);
-        Sort(WordsMas,words);
-        Output(WordsMas,c,k, v, words);
-        getch();
-        system("cls");
-    }
-    return 0;
+	setlocale(LC_ALL, "rus");
+	system("cls");
+	char TextMas[9999], WordsMas[100][50], c[4] = {',' , ' ' , '.' , '\n'};
+	int v = 0, k = 0, words = 0;
+	KeyInput(TextMas);
+	Menu1(TextMas);
+	Menu2(v, k);
+	Gaps(TextMas, c, WordsMas, words);
+	Sort(WordsMas, words);
+	Output(WordsMas, c, k, v, words);
+	system("pause");
 }
 
